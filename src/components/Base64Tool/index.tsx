4b1b0fb643a73be34e4a08d98720a4eadd0c8a10
Anthony Iacono <anthony.iacono@willowtreeapps.com>
2022-03-24 12:07:50 -0400

    Base64 tool example

--- src/components/Base64Tool/index.tsx
+++ src/components/Base64Tool/index.tsx
@@ -1,12 +1,21 @@
 import { Container, Text } from '../../styles/Common'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import {useMemo, useState} from "react";
+import  { Buffer } from 'buffer';
 
 export function Base64Tool() {
+    const [inputValue, setInputValue] = useState('');
+
+    const base64Value = useMemo(() => {
+        return Buffer.from(inputValue).toString('base64');
+    }, [inputValue]);
+
     return (
         <>
             <Container>
                 <Text>
-                    This is the base64 tool!
+                    <input type="text" placeholder="Encode me" onChange={e => setInputValue(e.target.value)} />
+                    <input type="text" readOnly value={base64Value} />
                     <Link to="/">Go home</Link>
                 </Text>
             </Container>
