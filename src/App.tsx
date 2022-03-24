7a68f4620f4cc632fef72b242945569cf54bf12f
Anthony Iacono <anthony.iacono@willowtreeapps.com>
2022-03-24 14:17:46 -0400

    UUID generation tool

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
 import { Home } from "./components/Home";
 import { Base64Tool } from "./components/Base64Tool";
 import { QRCodeTool } from "./components/QRCodeTool";
+import { UuidTool } from "./components/UuidTool";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { FileSearchTool } from './components/FileSearchTool';
 
@@ -17,6 +18,7 @@ export function App() {
                 <Routes>
                     <Route path="/" element={ <Home /> } />
                     <Route path="/base64" element={ <Base64Tool /> } />
+                    <Route path="/uuid" element={ <UuidTool /> } />
                     <Route path="/qrcode" element={ <QRCodeTool /> } />
                     <Route path="/filesearch" element={ <FileSearchTool /> } />
                 </Routes>
